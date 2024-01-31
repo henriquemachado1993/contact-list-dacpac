c88e403f1b4431b469016862289d6a3d7637dd57
Henrique Machado <henriquemachado1993@gmail.com>
2024-01-31 10:15:36 -0300

    Projeto com o projeto separado para DACPAC.

---
+++ code/ContactList/Data/ContactListContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContactList.Models;
+
+namespace ContactList.Data
+{
+    public class ContactListContext : DbContext
+    {
+        public ContactListContext (DbContextOptions<ContactListContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<ContactList.Models.Contacts> Contacts { get; set; }
+    }
+}
---
+++ code/ContactList/Models/Contacts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactList.Models
+{
+    public class Contacts
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+    }
+}
